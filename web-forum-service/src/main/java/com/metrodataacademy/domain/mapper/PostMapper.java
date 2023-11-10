bad8300bbeee518f39d7acf75e0062fe32c7c05b
Fairuz <fairuz.satria@ui.ac.id>
2023-11-10 07:14:23 +0700

    add author to get list post

--- web-forum-service/src/main/java/com/metrodataacademy/domain/mapper/PostMapper.java
+++ web-forum-service/src/main/java/com/metrodataacademy/domain/mapper/PostMapper.java
@@ -1,9 +1,13 @@
 package com.metrodataacademy.domain.mapper;
 
 import com.metrodataacademy.domain.dto.response.ResGetPostDto;
+import com.metrodataacademy.domain.dto.response.ResStagingUserDto;
 import com.metrodataacademy.domain.entity.Post;
+import com.metrodataacademy.domain.entity.StagingUser;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
 public interface PostMapper {
@@ -12,4 +16,15 @@ public interface PostMapper {
     @Mapping(target = "postId", source = "id")
     @Mapping(target = "threadsId", expression = "java(post.getThreads().getId())")
     ResGetPostDto postToResGetListPostDto(Post post);
+
+    @AfterMapping
+    default void mapStagingUserOfPostToResStagingUserDto(@MappingTarget ResGetPostDto resGetPostDto, Post post){
+        StagingUser stagingUser = post.getAuthor();
+        ResStagingUserDto resStagingUserDto = new ResStagingUserDto();
+
+        resStagingUserDto.setId(stagingUser.getName());
+        resStagingUserDto.setId(stagingUser.getId());
+
+        resGetPostDto.setAuthor(resStagingUserDto);
+    }
 }
